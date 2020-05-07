0260829d6b0bc2f5d752e2ab3a791617bb513c54
StadKolam <szecsitisza.adam@gmail.com>
2020-05-07 20:45:09 +0200

    Updates

---
+++ precache-manifest.8abc2853ca9dfa3bc3012180e921e2ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031812162109acf0bf9c9d0d89c55acd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e6a17ae68ef5d86d8c8a",
+    "url": "/monsters-rolodex/static/css/main.fba9f5fa.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6a17ae68ef5d86d8c8a",
+    "url": "/monsters-rolodex/static/js/main.1c134f49.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
